aac3e94595c657add6756d21c8b0401c365c37e3
Valtteri Nurminen <valtteri.e.nurminen@aalto.fi>
2019-01-12 21:05:49 +0200

    Form URLs, fetch JSON

--- restaurant_processor.js
+++ restaurant_processor.js
@@ -1,16 +1,49 @@
+/**
+ * FETCHES DATA FROM RESTAURANT APIS AND PROCESSES THE DATA INTO UNIFORM FORMAT.
+ */
+
 const request = require('request');
+const fs = require('fs');
+
+// Returns the date of Monday on the current week as YYYY-MM-dd.
+function getMonday(delimiter) {
+  const d = new Date();
+  const year = d.getFullYear().toString();
+  let month = (d.getMonth() + 1).toString().padStart(2, "0");
+  let day = (d.getDate() - (d.getDay() +6) % 7).toString().padStart(2, "0");
+
+  return year + delimiter + month + delimiter + day;
+}
+
 
 // Fetches the JSON data for the restaurant with given parameters.
-function getData(code, callback) {
-    request('https://www.fazerfoodco.fi/modules/json/json/Index?costNumber=0190&language=fi', (err, res, body) => {
+function getData(company, code, callback) {
+
+  let addr;
+  
+  if (company == "FAZER") {
+    addr = `https://www.fazerfoodco.fi/modules/json/json/Index?costNumber=${code}&language=fi&firstday=${getMonday("-")}`;
+  } else if (company == "SODEXO") {
+    addr = `https://www.sodexo.fi/ruokalistat/output/weekly_json/${code}/${getMonday("/")}/fi`;
+  }
+
+  request(addr, (err, res, body) => {
       if(err) { 
         console.log(err);
       } else {
-        callback(body);
+        callback(JSON.parse(body));
       }
-    }); 
+    });
 }
 
+// Updates the stored response.
+function updateResponses(data) {
+  fs.writeFile('./responses/test.txt', 'Lorem ipsum dolor sit amet...', (err, file) => console.log("Something happened"));
+}
+
+
+// EXPORTS
 module.exports = {
-  getData
+  getData,
+  updateResponses
 };
